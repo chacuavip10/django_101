e17456820380a7a627a40f8c350e181b76a017f8
chacuavip10 <chacuavip10@gmail.com>
2020-10-02 15:56:26 +0700

    name change

--- calc/views.py
+++ calc/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html', {'name': '123456789'})
+    return render(request, 'home.html', {'name': 'test'})
 # Create your views here.
 
 
