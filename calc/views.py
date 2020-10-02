74b75b9e1403e6118a3779060e6b148f1cffb1e1
chacuavip10 <chacuavip10@gmail.com>
2020-10-02 16:49:25 +0700

    > test

--- calc/views.py
+++ calc/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html', {'name': 'test123'})
+    return render(request, 'home.html', {'name': 'test'})
 # Create your views here.
 
 
